8e3d6184ed852abfca66d932383078692dde0b47
ramazan_kashipov <ramazan.kashipov@systeme.ru>
2023-08-22 17:55:37 +0300

    send logs to Telegram to admin users

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ from environs import Env
 import telegram
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot_token, chat_id):
+        super().__init__()
+        self.tg_bot = telegram.Bot(token=tg_bot_token)
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def send_message_by_bot(bot_credentials, checked_lessons):
     bot = telegram.Bot(token=bot_credentials['bot_token'])
     chat_id = bot_credentials['chat_id']
@@ -24,8 +36,6 @@ def main():
 
     env = Env()
     env.read_env()
-    if env.bool("LOGGING_DEBUG"):
-        logging.basicConfig(level=logging.DEBUG)
 
     telegram_user_token = env.str("TELEGRAM_USER_TOKEN")
     timestamp = None
@@ -33,6 +43,16 @@ def main():
         'bot_token': env.str("TELEGRAM_BOT_TOKEN"),
         'chat_id': env.int('TELEGRAM_CHAT_ID'),
     }
+    logging.basicConfig(format='%(message)s')
+    logger = logging.getLogger('telegram_bot')
+    if env.bool("LOGGING_DEBUG"):
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    logger.addHandler(TelegramLogsHandler(tg_bot_token=bot_credentials['bot_token'], chat_id=bot_credentials['chat_id']))
+
+    logger.info('Бот начал работу')
 
     while True:
         try:
@@ -43,7 +63,7 @@ def main():
             url = 'https://dvmn.org/api/long_polling/'
             timeout = 100
 
-            logging.debug(f'1. Timestamp is {timestamp}')
+            logger.debug(f'1. Timestamp is {timestamp}')
             payload = {'timestamp': timestamp}
             response = requests.get(url=url, headers=headers, timeout=timeout, params=payload)
 
@@ -53,14 +73,16 @@ def main():
             if checked_lessons["status"] == "timeout":
                 timestamp = checked_lessons["timestamp_to_request"]
             else:
+                timestamp = checked_lessons['last_attempt_timestamp']
                 send_message_by_bot(bot_credentials, checked_lessons['new_attempts'][0])
         except requests.HTTPError:
-            logging.error("Can't retrieve review list from API.")
+            logger.error("Can't retrieve review list from API.")
         except requests.exceptions.ReadTimeout:
-            logging.warning('Timeout reached out.')
+            logger.warning('Timeout reached out.')
         except requests.exceptions.ConnectionError:
-            logging.warning('Connection is broken.')
+            logger.warning('Connection is broken.')
             sleep(100)
+    logger.info('bot is shutdown')
 
 
 if __name__ == "__main__":
